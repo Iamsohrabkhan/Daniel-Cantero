71a69d4e08c110decfc3ae34d28a7f646f852425
Sohrab Khan <7ohrabkhan@gmail.com>
2026-01-27 21:20:09 +0500

    second comit

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ gsap.registerPlugin(ScrollTrigger, CustomEase);
 const lenis = new Lenis();
 lenis.on("scroll", ScrollTrigger.update);
 
-gsap.ticker.lagSmoothing(0);
-
 const navAnimations = () => {
   const menuButton = document.querySelector(".menu_button");
   if (menuButton) {
@@ -55,9 +53,12 @@ const navAnimations = () => {
 };
 
 const hoverListAnimations = () => {
+  const imageFollowHoverAnimationsContainer = document.querySelectorAll(
+    ".image_follow_hover_animations",
+  );
   const serviceWrappers = document.querySelectorAll(".service_wrapper");
 
-  serviceWrappers.forEach((servicewrapper) => {
+  serviceWrappers.forEach((servicewrapper, index) => {
     const listGridWrapper =
       servicewrapper.querySelectorAll(".list_grid_wrapper");
     const mouseFollowImage = servicewrapper.querySelectorAll(
@@ -94,11 +95,14 @@ const hoverListAnimations = () => {
       });
     });
 
-    servicewrapper.addEventListener("mousemove", (e) => {
-      const { clientX, clientY } = e;
-      xTo(clientX + 10);
-      yTo(clientY - 150);
-    });
+    imageFollowHoverAnimationsContainer[index].addEventListener(
+      "mousemove",
+      (e) => {
+        const { clientX, clientY } = e;
+        xTo(clientX + 20);
+        yTo(clientY - 150);
+      },
+    );
 
     listGridWrapper.forEach((curr, hoverIndex) => {
       curr.addEventListener("mouseenter", () => {
@@ -130,9 +134,134 @@ const hoverListAnimations = () => {
   });
 };
 
+// Archieve marqee animations
+
+const parallaxImagesAnimation = () => {
+  const parallaxImages = document.querySelectorAll(".parallax_image");
+  const processRevealImage = document.querySelectorAll(".process_reveal_image");
+
+  parallaxImages.forEach((curr) => {
+    const parallaxImageContainer = curr.closest(".parallax_image_container");
+    const rect = parallaxImageContainer.getBoundingClientRect();
+    const scrollProgress = rect.top / window.innerHeight;
+    const translateY = -scrollProgress * 150;
+    curr.style.transform = `translateY(${translateY}px)`;
+  });
+  processRevealImage.forEach((curr) => {
+    const parallaxImageContainer = curr.closest(
+      ".process_reveal_image_wrapper",
+    );
+    const rect = parallaxImageContainer.getBoundingClientRect();
+    const scrollProgress = rect.top / window.innerHeight;
+    const translateY = -scrollProgress * 150;
+    curr.style.transform = `translateY(${translateY}px)`;
+  });
+};
+
+const archieveMarqueeAnimations = () => {
+  const archieveMarquee = document.querySelectorAll(".archieve_marquee");
+  const archieveImageParallax = document.querySelectorAll(
+    ".archieve_image_parallax",
+  );
+
+  if (archieveMarquee.length < 2) return;
+
+  // initial states
+
+  gsap.set(archieveMarquee[1], {
+    x: -archieveMarquee[1].getBoundingClientRect().width / 3,
+  });
+  gsap.set(".archieve_text_animation_wrapper", {
+    scale: 0,
+  });
+  archieveImageParallax.forEach((curr, index) => {
+    if (index === 0) {
+      gsap.set(curr, {
+        scale: 0,
+        opacity: 0,
+        transformOrigin: "center",
+      });
+    } else {
+      gsap.set(curr, {
+        clipPath: "inset(50%)",
+      });
+    }
+  });
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".archieve_container",
+      start: "top top",
+      end: "80% bottom",
+      scrub: 1,
+      invalidateOnRefresh: true,
+      onEnter: () => {
+        console.log("entered");
+      },
+      onLeave: () => {
+        console.log("left");
+        gsap.to(".archieve_text_animation_wrapper", {
+          scale: 1,
+          // opacity: 1,
+          duration: 0.4,
+          delay: 0.2,
+        });
+      },
+      onEnterBack: () => {
+        console.log("reenter");
+        gsap.to(".archieve_text_animation_wrapper", {
+          scale: 0,
+          // opacity: 0,
+          duration: 0.4,
+        });
+      },
+      onLeaveBack: () => {
+        console.log("leave back");
+      },
+      // markers: true,
+    },
+  });
+
+  // marquee movement (runs across full scroll)
+  tl.to(
+    archieveMarquee[0],
+    {
+      x: () => -archieveMarquee[0].getBoundingClientRect().width / 3,
+      ease: "none",
+      willChange: "transform",
+    },
+    0,
+  );
+
+  tl.to(
+    archieveMarquee[1],
+    {
+      x: 0,
+      ease: "none",
+      willChange: "transform",
+    },
+    0,
+  );
+
+  // image scaling (side by side)
+  archieveImageParallax.forEach((curr, index) => {
+    tl.to(
+      curr,
+      {
+        scale: index === 0 ? 1 : 1,
+        clipPath: index !== 0 ? "inset(0%)" : null,
+        willChange: "transform",
+        ease: "none",
+      },
+      index === 0 ? ">" : ">",
+    );
+  });
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   navAnimations();
   hoverListAnimations();
+  archieveMarqueeAnimations();
 });
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -214,6 +343,7 @@ document.addEventListener("DOMContentLoaded", () => {
    =========================== */
 
   gsap.ticker.add((time, deltaTime) => {
+    parallaxImagesAnimation();
     lenis.raf(time * 1000);
     smoothVelocity *= VELOCITY_FRICTION;
 
@@ -231,4 +361,5 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
+  gsap.ticker.lagSmoothing(0);
 });
